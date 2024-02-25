cf0e46c9b7db264812365ad8dcfdc9f22f6398a4
Patrick Ma <patrickma6199@gmail.com>
2024-02-25 12:42:36 -0800

    added new php files to public/pages

---
+++ public/pages/profile.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head><!-- Other users profile -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../css/profile.css">
+    <link rel="icon" href="../img/logo.png">
+</head>
+<body>
+    <nav id="top-bar">
+        <a href="./discussionLoggedIn.html"><img src="../img/logo.png" alt="Pondr Logo" id="top-bar-logo"></a>
+        <div id="top-search-bar">
+            <form method="GET" action="discussionLoggedIn.html">
+                <input type="text" name="search" placeholder="Search for Users and Threads" />
+                <button type="submit" class="form-button">Search</button>
+            </form>
+        </div>
+        <a href="profile.html"><img src="../img/pfp-3.jpg" id="top-search-bar-pfp"> </a>
+    </nav>
+    <main class="column-container margin-down">
+        <section class="profile-container">
+            <div class="profile-img">
+                <a href="../img/pfp-2.jpg"><img src="../img/pfp-2.png" alt="profile picture"></a>
+            </div>
+            <div class="profile-text">
+                <p><b>Name:</b> Egg Fella</p> 
+                <p><b>Username:</b> EggFella1234</p>
+                <p> <b>Bio:</b> Hello! I am EggFella1234</p>
+                <p><span><b>Followers:</b> 1000  <b>Following:</b> 20</span></p>
+            </div>
+        </section>
+        <section class="discussion-container">
+            <h2>Their Threads</h2>
+            <div class="mini-thread">
+                <article>
+                    <a href="./thread.html"><h2>I love eating eggs!</h2></a>
+                    <i>Posted by: EggFella1234 on <time>January 1, 1970</time></i>
+                    <p>Eggs are so nutritious, and tasty I just want to eat eggs for every meal. </p>
+                </article>
+                <img src="../img/egg-1.jpg">
+            </div>
+           
+            <div class="mini-thread">
+                <article>
+                    <a href="./thread.html"><h2>Have y'all ever noticed how eggs are the perfect shape?</h2></a>
+                    <i>Posted by: EggFella1234 on <time>January 1, 1970</time></i>
+                    <p>Eggs just are the most perfect shape, I love how nice and smooth an egg feels on my hand!! It's just so cute and round.</p>
+                </article>
+                <img src="../img/egg-2.jpg">
+            </div>
+            
+            
+        </section>
+    </main>
+
+    
+    
+</body>
+</html>
---
+++ public/pages/thread_user.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sample Thread Name</title>
+    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../css/threads.css">
+    <link rel="icon" href="../img/logo.png">
+    <script src="https://kit.fontawesome.com/cfd53e539d.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <nav id="top-bar">
+        <a href="./discussionLoggedIn.html"><img src="../img/logo.png" alt="Pondr Logo" id="top-bar-logo"></a>
+        <div id="top-search-bar">
+            <form method="GET" action="discussionLoggedIn.html">
+                <input type="text" name="search" placeholder="Search for Users and Threads" />
+                <button type="submit" class="form-button">Search</button>
+            </form>
+        </div>
+        <a href="profile.html"><img src="../img/pfp-3.jpg" id="top-search-bar-pfp"> </a>
+    </nav>
+    <main class="column-container margin-down">
+        <div class="thread-container">
+            <article>
+                <img src="../img/cat.jpg" class="thread-img">
+                <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
+                <i>Posted by: <a href="./secondaryProfile.html">username</a> on <time>January 1, 1970</time> under Sports</i>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vitae nunc sed nisl finibus imperdiet. 
+                    Phasellus est tellus, sagittis quis tortor a, interdum congue massa. Praesent vitae varius nunc, sed ornar
+                    e arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vitae nunc sed nisl finibus imperdiet. Phasellus 
+                    est tellus, sagittis quis tortor a, interdum congue massa. Praesent vitae varius nunc, sed ornare arcu. Lorem ipsum dolor
+                    sit amet, consectetur adipiscing elit. Integer vitae nunc sed nisl finibus imperdiet. Phasellus est tellus, sagittis quis
+                    tortor a, interdum congue massa. Praesent vitae varius nunc, sed ornare arcu. Lorem ipsum dolor sit amet, consectetur
+                    adipiscing elit. Integer vitae nunc sed nisl finibus imperdiet. Phasellus est tellus, sagittis quis tortor a, interdum 
+                    congue massa. Praesent vitae varius nunc, sed ornare arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Integer vitae nunc sed nisl finibus imperdiet. Phasellus est tellus, sagittis quis tortor a, interdum congue massa.
+                    Praesent vitae varius nunc, sed ornare arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vitae nunc sed nisl finibus imperdiet.
+                    Phasellus est tellus, sagittis quis tortor a, interdum congue massa. Praesent vitae varius nunc, sed ornar
+                    e arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vitae nunc sed nisl finibus imperdiet.
+                    Phasellus
+                    est tellus, sagittis quis tortor a, interdum congue massa. Praesent vitae varius nunc, sed ornare arcu. Lorem ipsum
+                    dolor
+                    sit amet, consectetur adipiscing elit. Integer vitae nunc sed nisl finibus imperdiet. Phasellus est tellus, sagittis
+                    quis
+                    tortor a, interdum congue massa. Praesent vitae varius nunc, sed ornare arcu. Lorem ipsum dolor sit amet, consectetur
+                    adipiscing elit. Integer vitae nunc sed nisl finibus imperdiet. Phasellus est tellus, sagittis quis tortor a, interdum
+                    congue massa. Praesent vitae varius nunc, sed ornare arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Integer vitae nunc sed nisl finibus imperdiet. Phasellus est tellus, sagittis quis tortor a, interdum congue massa.
+                    Praesent vitae varius nunc, sed ornare arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+            </article>
+            <div id="icon-buttons">
+                <a href="" class="link-button"><i class="fa-solid fa-heart"></i> Like</a>
+                <a href="" class="link-button"><i class="fa-solid fa-comment"></i> Comment</a>
+                
+            </div>
+        </div>
+        <div class="thread-comments">
+            <article class="thread-comment-container">
+                <div class="thread-comment-profile">
+                    <img src="../img/stock_profile_photo.png" alt="profile photo">
+                    <i>username on <time>January 1, 1970</time></i>
+                </div>
+                <p class="thread-comment">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vitae nunc sed nisl
+                    finibus imperdiet. Phasellus est tellus, sagittis quis tortor a, interdum congue massa. Praesent vitae varius
+                    nunc, sed ornare arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vitae nunc sed nisl
+                    finibus imperdiet. Phasellus est tellus, sagittis quis tortor a, interdum congue massa. Praesent vitae varius
+                    nunc, sed ornare arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+                <div>
+                    <a href="" class="link-button" id="reply-icon"><i class="fa-solid fa-reply"></i> Reply</a>
+                </div>
+                
+                <div class="thread-comment-container">
+                    <div class="thread-comment-profile">
+                        <img src="../img/stock_profile_photo.png" alt="profile photo">
+                        <i>username on <time>January 1, 1970</time></i>
+                    </div>
+                        <p class="thread-comment">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vitae nunc sed nisl
+                            finibus imperdiet. Phasellus est tellus, sagittis quis tortor a, interdum congue massa. Praesent vitae varius
+                            nunc, sed ornare arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            </p>
+                        
+                            <div>
+                                <a href="" class="link-button" id="reply-icon"><i class="fa-solid fa-reply"></i> Reply</a>
+                            
+                            </div>
+                    
+                </div>
+                <div class="thread-comment-container">
+                    <div class="thread-comment-profile">
+                        <img src="../img/stock_profile_photo.png" alt="profile photo">
+                        <i>username on <time>January 1, 1970</time></i>
+                    </div>
+                    <p class="thread-comment">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vitae nunc sed nisl
+                        finibus imperdiet. Phasellus est tellus, sagittis quis tortor a, interdum congue massa. Praesent vitae varius
+                        nunc, sed ornare arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </p>
+                    <div>
+                        <a href="" class="link-button" id="reply-icon"><i class="fa-solid fa-reply"></i> Reply</a>
+                    
+                    </div>
+                </div>
+            </article>
+            <article class="thread-comment-container">
+                <div class="thread-comment-profile">
+                    <img src="../img/stock_profile_photo.png" alt="profile photo">
+                    <i>username on <time>January 1, 1970</time></i>
+                </div>
+                <p class="thread-comment">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vitae nunc sed nisl
+                    finibus imperdiet. Phasellus est tellus, sagittis quis tortor a, interdum congue massa. Praesent vitae varius
+                    nunc, sed ornare arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+                <div>
+                    <a href="" class="link-button" id="reply-icon"><i class="fa-solid fa-reply"></i> Reply</a>
+                
+                </div>
+                
+                <div class="thread-comment-container">
+                    <div class="thread-comment-profile">
+                        <img src="../img/stock_profile_photo.png" alt="profile photo">
+                        <i>username on <time>January 1, 1970</time></i>
+                    </div>
+                    <p class="thread-comment">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vitae nunc sed nisl
+                        finibus imperdiet. Phasellus est tellus, sagittis quis tortor a, interdum congue massa. Praesent vitae varius
+                        nunc, sed ornare arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </p>
+                    <div>
+                        <a href="" class="link-button" id="reply-icon"><i class="fa-solid fa-reply"></i> Reply</a>
+                    
+                    </div>
+                </div>
+            </article>
+            <article class="thread-comment-container">
+                <div class="thread-comment-profile">
+                    <img src="../img/stock_profile_photo.png" alt="profile photo">
+                    <i>username on <time>January 1, 1970</time></i>
+                </div>
+                <p class="thread-comment">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vitae nunc sed nisl
+                    finibus imperdiet. Phasellus est tellus, sagittis quis tortor a, interdum congue massa. Praesent vitae varius
+                    nunc, sed ornare arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vitae nunc sed nisl
+                    finibus imperdiet. Phasellus est tellus, sagittis quis tortor a, interdum congue massa. Praesent vitae varius
+                    nunc, sed ornare arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+                <div>
+                    <a href="" class="link-button" id="reply-icon"><i class="fa-solid fa-reply"></i> Reply</a>
+                
+                </div>
+            </article>
+            <article class="thread-comment-container">
+                <div class="thread-comment-profile">
+                    <img src="../img/stock_profile_photo.png" alt="profile photo">
+                    <i>username on <time>January 1, 1970</time></i>
+                </div>
+                <p class="thread-comment">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vitae nunc sed nisl
+                    finibus imperdiet. Phasellus est tellus, sagittis quis tortor a, interdum congue massa. Praesent vitae varius
+                    nunc, sed ornare arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vitae nunc sed nisl
+                    finibus imperdiet. Phasellus est tellus, sagittis quis tortor a, interdum congue massa. Praesent vitae varius
+                    nunc, sed ornare arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+                <div>
+                    <a href="" class="link-button" id="reply-icon"><i class="fa-solid fa-reply"></i> Reply</a>
+                
+                </div>
+            </article>
+        </div>
+    </main>
+</body>
+</html>
