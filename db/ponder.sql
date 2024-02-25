9d23b22664dfb6faac4152be23fda3326e683318
Patrick Ma <patrickma6199@gmail.com>
2024-02-25 12:03:38 -0800

    Finished initial ddl

--- db/ponder.sql
+++ db/ponder.sql
@@ -3,20 +3,23 @@
     May be altered at later dates to fit our potentially evolving requirements.
 */
 
--- CREATE DATABASE pondr; -- Use only in the first iteration to generate the database
+CREATE DATABASE pondr; -- Use only in the first iteration to generate the database
 USE ponder;
 
 CREATE TABLE users (
     userId INT AUTO_INCREMENT,
     utype BIT(1) NOT NULL, -- 0 if normal user, 1 if admin
-    fName VARCHAR(255) NOT NULL.
+    fName VARCHAR(255) NOT NULL,
     lName VARCHAR(255) NOT NULL,
     uName VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
     pass VARCHAR(255) NOT NULL,
     bio VARCHAR(1000) NOT NULL, -- will be "No Bio Made" by default
     pfp BLOB,
     PRIMARY KEY(id)
 ) ENGINE = InnoDB;
+
+-- Create new admin account
 
 CREATE TABLE posts (
     postId INT AUTO_INCREMENT,
@@ -27,6 +30,7 @@ CREATE TABLE posts (
     img BLOB,
     link VARCHAR(255) NOT NULL,
     catId INT NOT NULL,
+    likes INT NOT NULL DEFAULT(0);
     PRIMARY KEY(postId,userId),
     FOREIGN KEY(userId) REFERENCES users(userId)
         ON DELETE CASCADE,
@@ -36,9 +40,12 @@ CREATE TABLE posts (
 
 CREATE TABLE categories (
     catId INT AUTO_INCREMENT,
+    userId INT NOT NULL,
     name VARCHAR(255) NOT NULL,
     count INT NOT NULL DEFAULT(0), -- Count of posts under each category
-    PRIMARY KEY(catId)
+    PRIMARY KEY(catId),
+    FOREIGN KEY(userId) REFERENCES users(userId)
+        ON DELETE CASCADE
 ) ENGINE = InnoDB;
 
 CREATE TABLE comments (
@@ -46,7 +53,7 @@ CREATE TABLE comments (
     userId INT NOT NULL,
     postId INT NOT NULL,
     parentComId INT, -- if sub comment, user comment id, if not sub comment, enter null (can maybe come up with better method)
-    comDate DATETIME NOT NULL,
+    comDate DATETIME NOT NULL, -- same thing with GETDATE()
     text VARCHAR(3000) NOT NULL, -- 3000 character cap on dicussion comments'
     PRIMARY KEY(comId,userId),
     FOREIGN KEY(postId) REFERENCES posts(postId)
