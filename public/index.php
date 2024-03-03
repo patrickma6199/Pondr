6710d69e3886320d3278d469cfdb5e3004f64d24
Patrick Ma <patrickma6199@gmail.com>
2024-03-03 12:44:17 -0800

    add initial routing files

--- public/index.php
+++ public/index.php
@@ -1,1 +1,17 @@
 <!-- This will be the forwarding page/landing page. Should forward users depending on session variables to their respective discussion pages once button is clicked-->
+
+<?php
+    session_start();
+    $utype = $_SESSION['utype'];
+    $uid = $_SESSION['uid'];
+
+    switch ($utype) {
+    case 0:
+        header('Location: pages/discussion_user.php');
+        break;
+    case 1:
+        header('Location: pages/discussion_admin.php');
+    default:
+        header('Location: pages/index.php');
+    }
+?>
