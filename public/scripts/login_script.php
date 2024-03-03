be6d22d6cf6f036bb9bb8d31ce02af4d1ed1bcc5
Patrick Ma <patrickma6199@gmail.com>
2024-03-03 13:52:07 -0800

    added login script

---
+++ public/scripts/login_script.php
@@ -0,0 +1,41 @@
+<?php
+    session_start();
+    require_once('dbconfig.php');
+
+    $utype = $_SESSION['utype'];
+    $uid = $_SESSION['uid'];
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (!isset($utype)) {
+        //if the user is not logged in, verify theyre credentials and log them in if valid by setting session variables
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        //this is the sql query using prepared statements for sanitization of requests
+        $sql = "SELECT userId, utype, pass FROM users WHERE uName = ?";
+        $prstmt = $conn->prepare($sql);
+        $prstmt->bind_param($username);
+        $prstmt->execute();
+
+        $prstmt->bind_result($uid, $utype, $pass);
+
+        //if query only returns one user and user's password matches entered password
+        if ($prstmt->num_rows == 1 && $$password == $pass) {
+            //fetch() next available row
+            $prstmt->fetch();
+
+            //assign session variables
+            $_SESSION['utype'] = $utype;
+            $_SESSION['uid'] = $uid;
+
+            //redirect to discussions router
+            header('Location: ../index.php');
+        }
+
+    } else {
+        //reroutes user to discussion page router if they are logged in already
+        header('Location: ../index.php');
+    }
+}
+
+?>
