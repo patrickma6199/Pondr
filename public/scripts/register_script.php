e53870c22a1c48c8edcb96bad7a8c920f7a7300d
Patrick Ma <patrickma6199@gmail.com>
2024-03-03 15:45:03 -0800

    added initial functional code to insert new users into database

--- public/scripts/register_script.php
+++ public/scripts/register_script.php
@@ -8,34 +8,26 @@
 if (!isset($utype)) {
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-        //grab form information
+        //grab form information (required fields so will already be set)
         $firstName = $_POST['firstName'];
         $lastName = $_POST['lastName'];
+        $email = $_POST['email'];
         $username = $_POST['username'];
         $password = $_POST['password'];
 
+        //gets the string representation of the pfp sent through post request using temp filename stored on server from request
+        $pfp = (isset($_FILES['image'])) ? file_get_contents($_FILES['pfp']['tmp_name']):file_get_contents('../img/pfp.png');
+        //TODO: resize images to be standard size.
+
+
         //this is the sql query using prepared statements for sanitization of requests
-        $sql = "SELECT userId, utype, pass FROM users WHERE uName = ?";
+        $sql = "INSERT INTO users(utype, fName, lName, uName, email, pass, bio, pfp) VALUES (0, ?, ?, ?, ?, ?, 'No Bio Provided.', ?);";
         $prstmt = $conn->prepare($sql);
-        $prstmt->bind_param('s',$username);
-        $prstmt->execute();
+        $prstmt->bind_param('sssssb',$firstName,$lastName,$username,$email,$pass,$pfp);
 
-        $prstmt->bind_result($uid, $utype, $pass);
-
-        //if query only returns one user and user's password matches entered password
-        if ($prstmt->num_rows == 1 && $password == $pass) {
-            //fetch() next available row
-            $prstmt->fetch();
-
-            //assign session variables
-            $_SESSION['utype'] = $utype;
-            $_SESSION['uid'] = $uid;
-
-            $prstmt->close();
-            $conn->close();
-
-            //redirect to discussions router
-            header('Location: ../index.php');
+        //if query successful
+        if ($prstmt->execute()) {
+            //TODO: upload was successful
         }
     }
 } else {
