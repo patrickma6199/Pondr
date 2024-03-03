e290c67f55f1181405469abde4ae1051b73ada4a
prateek-balani <prateekbalani03@gmail.com>
2024-03-03 15:15:19 -0800

    initial commit

---
+++ public/scripts/like_button.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    var count = 0 ;
+
+    $("#like-button").on("click",function(e){
+        e.preventDefault();
+        count += 1;
+
+       var 
+       
+    });
+    
+});
