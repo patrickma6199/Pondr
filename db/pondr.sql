06d2c653219c012186f99a9060503421c9e3b169
prateek-balani <prateekbalani03@gmail.com>
2024-03-11 15:12:37 -0700

    merge changes

--- db/pondr.sql
+++ db/pondr.sql
@@ -22,7 +22,7 @@ CREATE TABLE users (
 -- INSERT INTO users() VALUES();
 INSERT INTO users(utype,fName,lName,uName,email,pass,bio) VALUES(0, 'John', 'Doe', 'john_doe', 'john.doe@example.com', 'password123', 'No Bio Made');
 INSERT INTO users(utype,fName,lName,uName,email,pass,bio) VALUES(0, 'Jane', 'Doe', 'jane_doe', 'jane.doe@example.com', 'securePass', 'No Bio Made');
-INSERT INTO users(utype,fName,lName,uName,email,pass,bio) VALUES(0, 'Mike', 'Smith', 'mike_smith', 'mike.smith@example.com', 'mike1234', 'No Bio Made'),;
+INSERT INTO users(utype,fName,lName,uName,email,pass,bio) VALUES(0, 'Mike', 'Smith', 'mike_smith', 'mike.smith@example.com', 'mike1234', 'No Bio Made');
 INSERT INTO users(utype,fName,lName,uName,email,pass,bio) VALUES(0, 'Emily', 'Jones', 'emily_jones', 'emily.jones@example.com', 'emilysPass', 'No Bio Made');
 INSERT INTO users(utype,fName,lName,uName,email,pass,bio) VALUES(0, 'Chris', 'Brown', 'chris_brown', 'chris.brown@example.com', 'chrisPass', 'No Bio Made');
 
@@ -51,7 +51,7 @@ CREATE TABLE posts (
     title VARCHAR(255) NOT NULL,
     text VARCHAR(3000) NOT NULL, -- 3000 character cap on discussion text
     img BLOB,
-    link VARCHAR(255) NOT NULL,
+    link VARCHAR(255) ,
     catId INT,
     likes INT NOT NULL DEFAULT(0),
     PRIMARY KEY(postId,userId),
@@ -60,11 +60,11 @@ CREATE TABLE posts (
     FOREIGN KEY(catId) REFERENCES categories(catId)
         ON DELETE CASCADE
 ) ENGINE = InnoDB;
-INSERT INTO posts(userId, title, text, link, catId, likes) VALUES(1, 'Post Title 1', 'Post Text 1', 'www.link1.com', 1, 0);
-INSERT INTO posts(userId, title, text, link, catId, likes) VALUES(2, 'Post Title 2', 'Post Text 2', 'www.link2.com', 2, 0);
-INSERT INTO posts(userId, title, text, link, catId, likes) VALUES(3, 'Post Title 3', 'Post Text 3', 'www.link3.com', 3, 0);
-INSERT INTO posts(userId, title, text, link, catId, likes) VALUES(4, 'Post Title 4', 'Post Text 4', 'www.link4.com', 4, 0);
-INSERT INTO posts(userId, title, text, link, catId, likes) VALUES(5, 'Post Title 5', 'Post Text 5', 'www.link5.com', 5, 0);
+INSERT INTO posts(userId, title, text, link, catId, likes, postDate) VALUES(1, 'Post Title 1', 'Post Text 1', 'www.link1.com', 1, 0, GETDATE());
+INSERT INTO posts(userId, title, text, link, catId, likes,postDate) VALUES(2, 'Post Title 2', 'Post Text 2', 'www.link2.com', 2, 0, GETDATE());
+INSERT INTO posts(userId, title, text, link, catId, likes,postDate) VALUES(3, 'Post Title 3', 'Post Text 3', 'www.link3.com', 3, 0, GETDATE());
+INSERT INTO posts(userId, title, text, link, catId, likes, postDate) VALUES(4, 'Post Title 4', 'Post Text 4', 'www.link4.com', 4, 0, GETDATE());
+INSERT INTO posts(userId, title, text, link, catId, likes,postDate) VALUES(5, 'Post Title 5', 'Post Text 5', 'www.link5.com', 5, 0, GETDATE());
 
 
 
