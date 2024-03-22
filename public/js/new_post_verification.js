f40744ed927f6e0e4f97ca5d40e416f7b1669b46
Patrick Ma <patrickma6199@gmail.com>
2024-03-22 10:17:07 -0700

    added subpage link verification

--- public/js/new_post_verification.js
+++ public/js/new_post_verification.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
     let link = $('input[name="post_link"]')
-    let validURL = /^https?:\/\/(?:\w+\.)+\w+$/;
+    const validURL = /^https?:\/\/(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(?:\/[^\/\s]*)*/;
     let error_mes = $("#error-postLink");
     let form = $('#post-form');
     let valid = /^$/;
     link.on('focusout', function() {
-        if (!validURL.test(link.val())) {
+        if (!validURL.test(link.val()) && link.val().length != 0) {
             error_mes.text("URL is invalid. Please follow this format: https://AAAAAA.AAA/AAA/AAA/").show();
         } else {
             error_mes.hide();
@@ -13,7 +13,7 @@ $(document).ready(function () {
     });
 
     form.on('submit', (e) => {
-        if (!validURL.test(link.val())) {
+        if (!validURL.test(link.val()) && link.val().length != 0) {
             e.preventDefault();
             error_mes.text("URL is invalid. Please follow this format: https://AAAAAA.AAA/AAA/AAA/").show();
         } else {
