5d7ccc436ebc720bf1c9d13ac1792515b77c766c
patrickma6199 <patrickma6199@gmail.com>
2024-03-25 15:23:14 -0700

    added permission checks to delete_posts

--- public/scripts/delete_posts.php
+++ public/scripts/delete_posts.php
@@ -1,39 +1,75 @@
 <?php
+
 session_start();
 ini_set('display_errors', 1);
 require_once './dbconfig.php';
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-$utype = isset ($_SESSION['utype']) ?? null;
-$uid = $_SESSION['uid'] ?? null;
 $utype = $_SESSION['utype'] ?? null;
+$uid = $_SESSION['uid'] ?? null;
 
-if ($utype == 1) {
-    if (isset ($_GET['postId'])) {
-        $pid = $_GET['postId'];
-        $uName = $_GET['uName'];
+if (isset ($_GET['postId'])) {
+    $pid = $_GET['postId'];
+    $conn->begin_transaction();
+    try{
+        //checking if user is permitted to delete
+        $sql = "SELECT userId FROM posts WHERE postId = ?;";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("i", $pid);
+        $stmt->bind_result($postUid);
+        $stmt->execute();
+        if ($stmt->fetch()) {
+            if ($uid == $postUid || $utype == 1) {
+                $allowed = true;
+            } else {
+                $allowed = false;
+            }
+        } else {
+            throw new Exception("Could not retrieve post uid.");
+        }
+        $stmt->close();
+        unset($stmt);
 
-        $sql = "DELETE FROM posts WHERE postId = ?";
-        try {
+        if ($allowed) {
+            $sql = "SELECT img FROM posts WHERE postId = ?";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("i", $pid);
+            $stmt->bind_result($postImg);
+            $stmt->execute();
+            if ($stmt->fetch()) {
+                if (isset ($postImg)) {
+                    if (!unlink($postImg)) {
+                        throw new Exception("Could not delete post image.");
+                    }
+                }
+            } else {
+                throw new Exception("Could not retrieve post image.");
+            }
+            $stmt->close();
+            unset($stmt);
+            $sql = "DELETE FROM posts WHERE postId = ?";
             $stmt = $conn->prepare($sql);
             $stmt->bind_param("i", $pid);
             $stmt->execute();
-            exit(header("Location: ../pages/profile.php?uName=$uName"));
-        } catch (mysqli_sql_exception $e) {
-            error_log("SQL Error: ", $e->getMessage());
-        } catch (Exception $e) {
-            error_log("SQL Error: ", $e->getMessage());
-        } finally {
-            if (isset ($stmt)) {
-                $stmt->close();
-            }
+            $conn->commit();
+        } else {
+            $conn->rollback();
+            exit(header("Location: ../index.php")); // user was not allowed to delete this post (wasn't their's/ wasn't logged in)
         }
-    } else {
-        error_log("POSTID NOT SET");
+    }catch (mysqli_sql_exception $e) {
+        $conn->rollback();
+        error_log("SQL Error: ", $e->getMessage());
+    } catch (Exception $e) {
+        $conn->rollback();
+        error_log("Error: ", $e->getMessage());
+    } finally {
+        if (isset ($stmt)) {
+            $stmt->close();
+        }
+        $conn->close();
     }
-    $conn->close();
+    exit(header("Location: ../pages/my_profile.php"));
 } else {
     $conn->close();
     exit (header("Location: ../index.php"));
 }
-
 ?>
