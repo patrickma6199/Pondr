d65ed1ce2a569f156669d87612c3bee7063493ab
patrickma6199 <patrickma6199@gmail.com>
2024-03-25 14:52:36 -0700

    added image deletion into deleting own posts

--- public/scripts/delete_my_posts.php
+++ public/scripts/delete_my_posts.php
@@ -8,17 +8,38 @@ $utype = isset ($_SESSION['utype']) ?? null;
 $uid = $_SESSION['uid'];
 
 if (isset ($_GET['postId'])) {
-    $pid = $_GET['postId'];
-    $sql = "DELETE FROM posts WHERE postId = ?";
+    $conn->begin_transaction();
     try{
+        $pid = $_GET['postId'];
+
+        $sql = "SELECT img FROM posts WHERE postId = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("i", $pid);
+        $stmt->bind_result($postImg);
+        $stmt->execute();
+        if ($stmt->fetch()) {
+            if (isset ($postImg)) {
+                if (!unlink($postImg)) {
+                    throw new Exception("Could not delete post image.");
+                }
+            }
+        } else {
+            throw new Exception("Could not retrieve post image.");
+        }
+        $stmt->close();
+        unset($stmt);
+        $sql = "DELETE FROM posts WHERE postId = ?";
         $stmt = $conn->prepare($sql);
         $stmt->bind_param("i", $pid);
         $stmt->execute();
+        $conn->commit();
         exit(header("Location: ../pages/my_profile.php"));
     }catch (mysqli_sql_exception $e) {
+        $conn->rollback();
         error_log("SQL Error: ", $e->getMessage());
     } catch (Exception $e) {
-        error_log("SQL Error: ", $e->getMessage());
+        $conn->rollback();
+        error_log("Error: ", $e->getMessage());
     } finally {
         if (isset ($stmt)) {
             $stmt->close();
