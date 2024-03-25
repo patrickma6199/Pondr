184e39b07fa050b70a2ba2680ff27639c3a5b79b
Patrick Ma <patrickma6199@gmail.com>
2024-03-24 18:40:16 -0700

    added check for uid

--- public/js/like_button.js
+++ public/js/like_button.js
@@ -1,41 +1,43 @@
 $(document).ready(function() {
 
     setInterval(getLikeCount, 5000);
-    updateLikeButton();
-    getLikeCount();
-   
-    $('#like-button').click(function(e) {
-        e.preventDefault();
-        const pid_param = new URLSearchParams(window.location.search);
-        var postId = pid_param.get('postId'); 
-        const action = 'like'; 
+    if (loggedIn) {
+        updateLikeButton();
+        getLikeCount();
+    
+        $('#like-button').click(function(e) {
+            e.preventDefault();
+            const pid_param = new URLSearchParams(window.location.search);
+            var postId = pid_param.get('postId'); 
+            const action = 'like'; 
 
-        $.ajax({
-            type: 'POST',
-            url: '../scripts/likes.php',
-            data: {postId: postId, action: action},
-            success: function (data) {
-                if (data.error !== undefined) {
-                    console.log(data.error);
-                }
-                else {
-                    if (data.success) {
-                        console.log("liked successfully!");
-                        if (data.liked) {
-                            $('#like-button').addClass('liked');
-                        } else {
-                            $('#like-button').removeClass('liked');
-                        }
+            $.ajax({
+                type: 'POST',
+                url: '../scripts/likes.php',
+                data: {postId: postId, action: action},
+                success: function (data) {
+                    if (data.error !== undefined) {
+                        console.log(data.error);
                     }
-                    getLikeCount();
+                    else {
+                        if (data.success) {
+                            console.log("liked successfully!");
+                            if (data.liked) {
+                                $('#like-button').addClass('liked');
+                            } else {
+                                $('#like-button').removeClass('liked');
+                            }
+                        }
+                        getLikeCount();
+                    }
+                },
+                error: function(xhr, status, error) {
+                    console.error('Error:', error);
+                    console.log("status:", status);
                 }
-            },
-            error: function(xhr, status, error) {
-                console.error('Error:', error);
-                console.log("status:", status);
-            }
+            });
         });
-    });
+    }
 });
         
 function getLikeCount(){
